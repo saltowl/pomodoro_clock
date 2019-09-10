2b5ac6260abe16eed8ed55f8c480c759d83549f9
saltowl <vladislava-shakirova@mail.ru>
2019-09-10 15:57:25 +0300

    Updates

---
+++ precache-manifest.5fa4b07b67cd4c1728804e75df786798.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e58debdc5a7aa5d4570da27e07e26c",
+    "url": "/PomodoroClock/index.html"
+  },
+  {
+    "revision": "c274d216d89b0b78b123",
+    "url": "/PomodoroClock/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "28510b33ff29a14f1c4e",
+    "url": "/PomodoroClock/static/css/main.a468a030.chunk.css"
+  },
+  {
+    "revision": "c274d216d89b0b78b123",
+    "url": "/PomodoroClock/static/js/2.d913a771.chunk.js"
+  },
+  {
+    "revision": "28510b33ff29a14f1c4e",
+    "url": "/PomodoroClock/static/js/main.9b9b0f4d.chunk.js"
+  },
+  {
+    "revision": "f685a2500feeb9c0e4e9",
+    "url": "/PomodoroClock/static/js/runtime~main.83403a32.js"
+  }
+]);
